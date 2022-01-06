8028f699d09fc4b54f5dcd85c90b9973855c644e
JainShubham23 <shubhamjain3815@gmail.com>
2022-01-05 20:29:05 -0500

    Updates

---
+++ precache-manifest.5c557309e5c7524dd774dcc49dbf8ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd142948d187cdc37aace7ccbe95b457",
+    "url": "/college-predictor/index.html"
+  },
+  {
+    "revision": "9a17e422044b8546f053",
+    "url": "/college-predictor/static/css/main.7d126119.chunk.css"
+  },
+  {
+    "revision": "5104b41ddb0e55b333e4",
+    "url": "/college-predictor/static/js/2.20028444.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/college-predictor/static/js/2.20028444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a17e422044b8546f053",
+    "url": "/college-predictor/static/js/main.1bf22206.chunk.js"
+  },
+  {
+    "revision": "63c336307a1c15e8a25b",
+    "url": "/college-predictor/static/js/runtime-main.91f40e9e.js"
+  }
+]);
